9b178411c6fb31cbeed9826ecd7a222594e81830
ahad <ahadas2005@gmail.com>
2024-10-20 14:31:33 +0300

    Initial Commit

---
+++ wwater/wwaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwaterApp.swift
+//  wwater
+//
+//  Created by ahad alshehah on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct wwaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wwaterTests/wwaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  wwaterTests.swift
+//  wwaterTests
+//
+//  Created by ahad alshehah on 17/04/1446 AH.
+//
+
+import Testing
+@testable import wwater
+
+struct wwaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
